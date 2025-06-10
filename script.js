a7781f4663efbae7d542671c8de8d05b40e62e5d
= <=>
2025-06-10 16:29:32 +0530

    added player images

--- script.js
+++ script.js
@@ -20,20 +20,37 @@ $('.brick').addClass('pos-abs');
 }
 
 
+
 let flag = 0;
-$('.brick').on('click', function () {
-      const windowHeight = $(window).height();
-      const currentTop = $(this).offset().top;
 
-      $(this).animate({
-        top: windowHeight + 200 // move out of view
-      }, 800, 'easeInQuad', function () {
-        // Optional: remove or reset
+
+$('.brick').on('click', function() {
+    const windowHeight = $(window).height();
+    const clickedBrick = $(this);
+    
+    // Animate the clicked brick
+    clickedBrick.animate({
+        top: windowHeight + 200
+    }, 800, 'easeInQuad', function() {
         $(this).hide();
-      });
-      flag ++;
+    });
+    
+    // Get all other bricks except the clicked one
+    const otherBricks = $('.brick').not(clickedBrick);
+    
+    // Select 3 random bricks (or fewer if there aren't enough)
+    const randomBricks = getRandomElements(otherBricks, 3);
+    
+    // Animate each random brick
+    randomBricks.each(function() {
+        $(this).animate({
+            top: windowHeight + 200
+        }, 800, 'easeInQuad', function() {
+            $(this).hide();
+        });
+              flag ++;
 
-      if (flag == 10){
+      if (flag == 18){
             $('.brick-intro').animate({
                  top: windowHeight + 200 // move out of view
             }, 800, 'easeInQuad', function () {
@@ -42,5 +59,28 @@ $('.brick').on('click', function () {
       });
       }
     });
+});
+
+// Helper function to get random elements from a jQuery collection
+function getRandomElements(elements, count) {
+    const shuffled = elements.toArray().sort(() => 0.5 - Math.random());
+    return $(shuffled.slice(0, Math.min(count, shuffled.length)));
+}
+
+
+
+
+
+// $('.brick').on('click', function () {
+//       const windowHeight = $(window).height();
+//       const currentTop = $(this).offset().top;
+
+//       $(this).animate({
+//         top: windowHeight + 200 // move out of view
+//       }, 800, 'easeInQuad', function () {
+//         // Optional: remove or reset
+//         $(this).hide();
+//       });
+//     });
 
 });
